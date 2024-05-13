f4b8b15e99e7c88cb43f6c0488b308d89f2a2910
areumH <94692566+areumH@users.noreply.github.com>
2024-05-13 22:12:19 +0900

    feat: 시작 버튼 클릭 시 배열 셔플 기능 구현

--- onetofifty/src/app/Components/Container/Container.tsx
+++ onetofifty/src/app/Components/Container/Container.tsx
@@ -3,14 +3,18 @@
 import React from 'react';
 import Cell from './Cell';
 
-export default function Container(): React.ReactNode {
-  const numbersArray: number[] = Array.from({ length: 25 }, (_, index) => index + 1);
+interface ContainerProps {
+  array: number[];
+}
 
+const Container: React.FC<ContainerProps> = ({ array }) => {
   return (
     <div className="grid grid-cols-5 grid-rows-5 p-2 gap-1.5 w-[400px] h-[400px] bg-blue-500 rounded-xl">
-      {numbersArray.map((el) => (
+      {array.map((el) => (
         <Cell num={el} />
       ))}
     </div>
   );
-}
+};
+
+export default Container;
--- onetofifty/src/app/page.tsx
+++ onetofifty/src/app/page.tsx
@@ -1,11 +1,36 @@
 'use client';
 
-import Container from "./Components/Container/Container";
+import { useState } from 'react';
+import Container from './Components/Container/Container';
 
 export default function Main() {
+  const numbersArray: number[] = Array.from({ length: 25 }, (_, index) => index + 1);
+  const [array, setArray] = useState<number[]>(numbersArray);
+
+  const shuffleArray = (array: number[]) => {
+    const shuffled: number[] = [];
+
+    while (array.length !== 0) {
+      const index = Math.floor(Math.random() * array.length);
+      shuffled.push(array[index]);
+      array.splice(index, 1);
+    }
+    return shuffled;
+  };
+
+  const handleStartButton = () => {
+    setArray(shuffleArray(numbersArray));
+  };
+
   return (
-    <div className="flex flex-col w-10/12 h-svh justify-center items-center">
-      <Container />
+    <div className="flex flex-col w-10/12 h-svh justify-center items-center gap-12">
+      <Container array={array} />
+      <button
+        className="w-28 h-12 bg-gray-900 text-white text-lg rounded-xl hover:bg-opacity-80"
+        onClick={handleStartButton}
+      >
+        게임 시작
+      </button>
     </div>
   );
 }
