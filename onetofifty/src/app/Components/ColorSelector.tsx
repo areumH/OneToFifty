a6b52305f3efb753415525354a764198c13b56ea
areumH <94692566+areumH@users.noreply.github.com>
2024-05-20 21:31:17 +0900

    design: 색상 선택 박스 UI 구현

---
+++ onetofifty/src/app/Components/ColorSelector.tsx
@@ -0,0 +1,9 @@
+const ColorSelector = () => {
+  return (
+    <div className="flex w-72 h-36 items-center px-7 border-2 border-gray-400 rounded-xl hover:shadow-md transition duration-200 ease-in-out">
+      <div className="w-20 h-20 border-8 border-violet-400 rounded-2xl bg-sky-300"></div>
+    </div>
+  );
+};
+
+export default ColorSelector;
--- onetofifty/src/app/setting/page.tsx
+++ onetofifty/src/app/setting/page.tsx
@@ -1,9 +1,18 @@
 'use client';
 
+import ColorSelector from "../Components/ColorSelector";
+
 export default function Setting() {
   return (
-    <div className="flex flex-col w-10/12 h-svh bg-test-color-100 text-blue-500">
-      Setting 설정 페이지
+    <div className="flex w-10/12 h-svh justify-center relative py-10">
+      <div className="flex flex-col items-center gap-7">
+        <div className="text-xl font-bold">COLOR</div>
+        <div className="flex flex-col gap-5">
+          <ColorSelector />
+          <ColorSelector />
+          <ColorSelector />
+        </div>
+      </div>
     </div>
   );
 }
