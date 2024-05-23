4fb96f3d342e443a640f69dcdbe638d8017f0ffa
areumH <94692566+areumH@users.noreply.github.com>
2024-05-23 21:33:55 +0900

    feat: 헤더 로고 클릭 시 게임 페이지로 이동

--- onetofifty/src/app/Components/Header.tsx
+++ onetofifty/src/app/Components/Header.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import Link from 'next/link';
 import ThemeSwitcher from './ThemeSwitcher';
 
 export default function Header() {
   return (
     <div className="flex justify-between w-10/12 items-center h-16 px-2 border-b border-gray-300">
       <p className="text-base">areumH</p>
-      <h3 className="text-lg font-bold">ONETOFIFTY</h3>
+      <Link href="/">
+        <h3 className="text-lg font-bold">ONETOFIFTY</h3>
+      </Link>
       <ThemeSwitcher />
     </div>
   );
